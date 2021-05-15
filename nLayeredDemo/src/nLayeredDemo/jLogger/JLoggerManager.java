d0144bf08a76bbf8babbb4b5cb4a2a374bd06619
Kübra Nur BAYINDIR <83094285+kubranurbayindir@users.noreply.github.com>
2021-05-15 16:26:17 +0300

    Add files via upload

---
+++ nLayeredDemo/src/nLayeredDemo/jLogger/JLoggerManager.java
@@ -0,0 +1,11 @@
+package nLayeredDemo.jLogger;
+
+public class JLoggerManager {
+	public void log(String message) {
+		System.out.println("J Logger ile loglandý.");
+		
+	}//sisteme dýþardan yazýlan bir paket olarak bunu düþünürsek bunu sisteme ekliyecez bu paketi
+	//mikroservice mimarileride bu mantýkla sisteme enjekte edilir. 
+
+
+}
